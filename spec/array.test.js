62415eed06402fd1540c1642c069be9314dcea63
Kevin Yang <chgc_kevin@hotmail.com>
2018-09-26 11:42:40 +0800

    新增 array 類型題目

---
+++ spec/array.test.js
@@ -0,0 +1,22 @@
+describe('array', () => {
+  const array = require('../src/array');
+  let data;
+  beforeEach(() => {
+    data = [0, 1, 2, 3, 4, 5];
+  });
+
+  it('取得只有奇數的陣列資料', () => {
+    expect(array.onlyOddNumber(data)).toEqual([1, 3, 5]);
+  });
+
+  it('取得中間 2 位的資料', () => {
+    expect(array.takeMiddleNumer(data, 2)).toEqual([2, 3]);
+    expect(array.takeMiddleNumer(data, 3)).toEqual([2, 3, 4]);
+    expect(array.takeMiddleNumer(data, 4)).toEqual([1, 2, 3, 4]);
+  });
+
+  it('取最大值', () => {
+    const mockData = [3, 5, -1, 39, 1, 4, 97, 42, 33];
+    expect(array.getMaxNumber(mockData)).toEqual(97);
+  });
+});
---
+++ src/array.js
@@ -0,0 +1,16 @@
+function onlyOddNumber(data) {
+  return [];
+}
+function takeMiddleNumer(data, count) {
+  return [];
+}
+
+function getMaxNumber(data) {
+  return 0;
+}
+
+module.exports = {
+  onlyOddNumber,
+  takeMiddleNumer,
+  getMaxNumber
+};
