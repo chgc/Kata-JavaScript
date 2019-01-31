ea27a4520cafda9cdff3d433de15e0ac4ea7d050
Jimmy <jiaming0708@gmail.com>
2019-01-31 15:01:46 +0800

    padRight test case
    string should include `padLeft` and `padRight` at same time

--- spec/string.test.ts
+++ spec/string.test.ts
@@ -9,4 +9,14 @@ describe('string', () => {
       expect(string.padLeft('123', 8, '0')).toEqual('00000123');
     });
   });
+
+  describe('padRight', () => {
+    it('文字-右邊填0', () => {
+      expect(string.padRight('demo', 8, '0')).toEqual('demo0000');
+    });
+
+    it('數字', () => {
+      expect(string.padRight('123', 8, '0')).toEqual('12300000');
+    });
+  });
 });
